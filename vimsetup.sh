7a1b7fb71d14327f4535e7bc8af98abf64bf90db
Robert Freiberger <robertf@yahoo-inc.com>
2016-10-25 00:43:28 -0700

    Testing first version of vimsetup

---
+++ vimsetup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Robert Freiberger
+# 10/24/2016
+
+function check_vim_dirs {
+  if [ -d "$1" ]
+  then
+    echo "Found the $1 directory"
+  else
+    echo "You are missing the $1 directory"
+    echo  -e "\033[33;5mCreating the $1 directory\033[0m"
+    create_vim_dirs $1
+
+  fi
+}
+
+function create_vim_dirs {
+  mkdir $1
+  if [ -d "$1" ]
+  then
+    echo "Directory $1 created!"
+  else
+    echo "Failed to create $1 directory, exiting"
+    exit 1
+  fi
+}
+
+function check_vim_version {
+  VIM_VER=`vim --version 2&1>/dev/null`
+  if [ $? -eq 0 ]
+  then
+    echo "Vim is installed"
+  else
+    echo "Vim is not installed"
+    if
+  fi
+}
+
+check_vim_version
+
+function copy_vim_files {
+  echo "Copying over the vimrc file"
+  wget -P $HOME https://raw.githubusercontent.com/rfreiberger/Vim-Setup/master/.vimrc
+  echo "Done copying over vimrc"
+  echo "Copying over the plugin files"
+  wget -P $HOME/.vim/colors/molokai https://raw.githubusercontent.com/rfreiberger/Vim-Setup/master/.vim/colors/molokai.vim
+  echo "Done copying over colors"
+}
+
+check_vim_version
+
+check_vim_dirs $HOME/.vim
+check_vim_dirs $HOME/.vim/autoload
+check_vim_dirs $HOME/.vim/backup
+check_vim_dirs $HOME/.vim/bundle
+check_vim_dirs $HOME/.vim/colors
+check_vim_dirs $HOME/.vim/plugged
+check_vim_dirs $HOME/.vim/swap
+check_vim_dirs $HOME/.vim/undo
+
+copy_vim_files
